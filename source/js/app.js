3ccaa2985299bb270844c11389056df3e96cf90c
Nick Bravo <nick@nickbravo.dev>
2022-05-29 21:41:58 -0600

    Version 1.0

---
+++ source/js/app.js
@@ -0,0 +1,34 @@
+// Form validation
+(function() {
+  'use strict';
+  const forms = document.querySelectorAll('.needs-validation');
+  Array.from(forms)
+    .forEach(function(form) {
+      form.addEventListener('submit', function(event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        } else if (form.checkValidity() == true) {
+          event.preventDefault();
+          form.submit();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+})();
+
+// Contact form success popup
+$(document).ready(function() {
+  if (window.location.href.indexOf('#success') != -1) {
+    $('#success').modal('show');
+    // Dismiss modal after 3 seconds if no input
+    setTimeout(function() {
+      $("#success").modal('hide');
+    }, 3000);
+  }
+});
+
+// Prevent form from resubmitting on refresh
+if (window.history.replaceState) {
+  window.history.replaceState(null, null, window.location.href);
+}
